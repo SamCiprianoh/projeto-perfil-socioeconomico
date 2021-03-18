66dff3d48b7d59a43aed5e05de92f31ec4a1cb5e
Samuel Cipriano <samciprianoh@gmail.com>
2021-03-18 03:32:47 -0300

    Adicionado tabela de email e RA

--- js/chart.js
+++ js/chart.js
@@ -1,25 +1,25 @@
 //LÊ O ARQUIVO JSOM
 async function generateChart() {
-   const file = await fetch('js/forms.json')
-
-   const json = await file.json()
-   const forms = json.forms
+    const file = await fetch('js/forms.json')
+ 
+    const json = await file.json()
+    const forms = json.forms
+    
+    let email = [], RA = [], i = 0
+    let cabecalho = '', dadosTabela = ''
    
-   let email = [], RA = [], i = 0
-   let cabecalho = '', dadosTabela = ''
-  
-   cabecalho = '<tr class="thead__row"><td class="thead__data">E-mail</td> <td class="thead__data">RA</td></tr>'
-   document.getElementById('thead').innerHTML = cabecalho
-
-   for(quest of forms){
-       email[i] = quest["Email Address"]
-       RA[i] = quest["3. Informe os 7 últimos dígitos do seu RA (109048xxxxxxx)"]
-       
-       dadosTabela = dadosTabela + `<tr class="tbody__row"><td class="tbody__data">${email[i]}</td><td class="tbody__data">${'109048' + RA[i]}</td></tr>`
-       i++
-   }
-
-   document.getElementById('tbody').innerHTML = dadosTabela
-}
-
-generateChart()
+    cabecalho = '<tr class="thead__row"><td class="thead__data">E-mail</td> <td class="thead__data">RA</td></tr>'
+    document.getElementById('thead').innerHTML = cabecalho
+ 
+    for(quest of forms){
+        email[i] = quest["Email Address"]
+        RA[i] = quest["3. Informe os 7 últimos dígitos do seu RA (109048xxxxxxx)"]
+        
+        dadosTabela = dadosTabela + `<tr class="tbody__row"><td class="tbody__data">${email[i]}</td><td class="tbody__data">${'109048' + RA[i]}</td></tr>`
+        i++
+    }
+ 
+    document.getElementById('tbody').innerHTML = dadosTabela
+ }
+ 
+ generateChart()
